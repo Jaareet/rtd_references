aa382ed3d15cab56a5e23c9ef800ca38946d4abe
RTDTonino <85588992+RTDTonino@users.noreply.github.com>
2022-04-17 13:18:35 -0400

    feat: function/server

--- server/main.lua
+++ server/main.lua
@@ -5,7 +5,7 @@ function DeleteBlip(source, CurrentAsign)
     local Player = ESX.GetPlayerFromId(source)
     local name = Player.getName()
     for k,v in pairs(polices) do
-        TriggerClientEvent('rtd_refuerzos:deleteRef', k, source, CurrentAsign, name)
+        TCE('rtd_refuerzos:deleteRef', k, source, CurrentAsign, name)
     end
     referencias[source] = nil
 end
@@ -17,19 +17,19 @@ function CreateBlip(source, color, CurrentAsign)
     local pos = GetEntityCoords(playerPed)
     local heading = GetEntityHeading(playerPed)
     for k,v in pairs(polices) do
-        TriggerClientEvent('rtd_refuerzos:setRef', k, source, pos, color, heading, CurrentAsign, name)
+        TCE('rtd_refuerzos:setRef', k, source, pos, color, heading, CurrentAsign, name)
     end
     referencias[source] = true
 end
 
-RegisterServerEvent('rtd_refuerzos:setRef')
-AddEventHandler('rtd_refuerzos:setRef', function(type, CurrentAsign)
+RSE('rtd_refuerzos:setRef')
+ADH('rtd_refuerzos:setRef', function(type, CurrentAsign)
     local source = source
 
     if(not polices[source]) then return end 
     
     if referencias[source] then 
-        xPlayer.showNotification("Has desactivado los ~o~ refuerzos")
+        SNS("Has desactivado los ~o~ refuerzos")
         DeleteBlip(source, CurrentAsign)
     end
 
@@ -39,7 +39,7 @@ AddEventHandler('rtd_refuerzos:setRef', function(type, CurrentAsign)
 
 end)
 
-Citizen.CreateThread(function()
+CT(function()
     while true do
         local mrs = 1200
         for k,v in pairs(referencias) do
@@ -47,26 +47,26 @@ Citizen.CreateThread(function()
             local policeCoords = GetEntityCoords(policePed)
             local policeHeading = GetEntityHeading(policePed)        
             for _,val in pairs(polices) do
-                TriggerClientEvent('rtd_refuerzos:setRef', _, k, policeCoords, 3, policeHeading, "", "")
+                TCE('rtd_refuerzos:setRef', _, k, policeCoords, 3, policeHeading, "", "")
             end
         end
-        Citizen.Wait(mrs)
+        W(mrs)
     end
 end)
 
-AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
+ADH('esx:playerLoaded', function(playerId, xPlayer)
     if(xPlayer and xPlayer.job.name == "police") then
         polices[playerId] = true
     end
 end)
 
-AddEventHandler('esx:playerDropped', function(playerId)
+ADH('esx:playerDropped', function(playerId)
     if polices[playerId] then
         polices[playerId] = nil
     end
 end)
 
-AddEventHandler('esx:setJob', function(source, job)
+ADH('esx:setJob', function(source, job)
     if(polices[source] and job.name ~= "police") then
         polices[source] = nil
     end
@@ -76,7 +76,7 @@ AddEventHandler('esx:setJob', function(source, job)
     end
 end)
 
-AddEventHandler('onServerResourceStart', function(name)
+ADH('onServerResourceStart', function(name)
     if GetCurrentResourceName() == name then
         local xPlayers = ESX.GetPlayers()
 

04a375b2a36ce32663aab3d3a47af3f1a7f690d4
RTDTonino <85588992+RTDTonino@users.noreply.github.com>
2022-06-11 11:58:22 -0400

    feat: last release

--- client/main.lua
+++ client/main.lua
@@ -1,12 +1,3 @@
-ESX = nil
-
-Citizen.CreateThread(function()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    W(30)
-  end
-end)
-
 local CA = "Sin Asignación"
 
 
